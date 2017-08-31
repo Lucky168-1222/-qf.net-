c51ef35c51ff4687f5605fd8f8a2819c987c03bf
wanlpenl <a769844601@qq.com>
2017-08-31 15:40:00 +0800

    上线总结

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -6,7 +6,7 @@ use app\index\model\User;
 use app\index\model\Plate;
 use app\index\model\Shops;
 use app\index\controller\Auth;
-//网站首页
+// //网站首页
 class Index extends Auth
 {
 	//在线商城首页
@@ -19,28 +19,5 @@ class Index extends Auth
 		$this->head();
 		return $this->fetch();	
 	}
-
-
-
-	// //head头分布页面
-	// public function head()
-	// {
-	// 	$sess = Session::get('user_id');
-	// 	$user = new User();
-	// 	$u_name =  $user::homeHead($sess);
-	// 	if (!empty($u_name)) {
-	// 		$u_name = $u_name[0]['user_name'];
-	// 		session('user_name',$u_name);
-	// 	}
-
-	// 	$plate =  new Plate();
-	// 	$data = $plate->actiontree();
-	// 	// dump($data);exit;
-	// 	$this->assign('data',$data);
-	// 	return $this->fetch();
-	// }
-
-	
-
 
 }
